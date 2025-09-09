2a2fe62bf2f08b4503a17053e67ad58d038f5ac0
RazSoft <razgroupgame@gmail.com>
2025-09-09 08:47:30 +0530

    Initial project setup complete with the added base component file of Navbar and Hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full flex">
+      <ul className="flex">
+        <li>LOGO</li>
+        <li>
+          <ul className="flex">
+            <li>Work</li>
+            <li>Services</li>
+            <li>Clients</li>
+            <li>About</li>
+            <li>Knowledge</li>
+            <li>Contact</li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+}
