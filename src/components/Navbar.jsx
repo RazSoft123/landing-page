01557ac7de3acf2e26c348d61c5e401755eafed8
RazSoft <razgroupgame@gmail.com>
2025-09-09 09:13:34 +0530

    Add basic style to navbar. making bottom border black and contact button background black.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 export default function Navbar() {
   return (
-    <nav className="w-full flex">
-      <ul className="flex">
-        <li>LOGO</li>
-        <li>
-          <ul className="flex">
+    <nav className="w-full flex fixed items-center justify-center py-4 px-4 border-b-2">
+      <ul className="flex xl:w-[1024px] items-center justify-between">
+        <li className="text-4xl font-bold py-1">LOGO</li>
+        <li className="min-h-full">
+          <ul className="flex items-center xl:gap-8 min-h-full font-medium">
             <li>Work</li>
             <li>Services</li>
             <li>Clients</li>
             <li>About</li>
             <li>Knowledge</li>
-            <li>Contact</li>
+            <li className="">
+              <a className="text-xl font-bold text-white bg-black px-8 py-3 rounded-sm">
+                Contact
+              </a>
+            </li>
           </ul>
         </li>
       </ul>
